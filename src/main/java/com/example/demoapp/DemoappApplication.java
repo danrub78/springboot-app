2097cb928af5f5042164f68b98209d331a9379a0
Dante Rubinaccio <dante@MBP-di-Dante.home>
2022-01-12 15:50:47 +0100

    modify file

--- src/main/java/com/example/demoapp/DemoappApplication.java
+++ src/main/java/com/example/demoapp/DemoappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoappApplication {
 
 	public static void main(String[] args) {
-		//spring run
+		//modify
 		SpringApplication.run(DemoappApplication.class, args);
 	}
 
